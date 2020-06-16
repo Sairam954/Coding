bf5b8aabb70900fb3818219d48706c86685da831
sairam <sairamvgraju@gmail.com>
2020-06-16 11:39:56 -0400

    Coding

---
+++ Hackerrank/Arrays/ArrayRotation.java
@@ -0,0 +1,72 @@
+package com.hackerrank.arrays;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ArrayRotation {
+
+	// Complete the rotLeft function below.
+	static int[] rotLeft(int[] a, int d) {
+		int start = 0;
+		int end = a.length - 1;
+		reverse(a, start, end);
+		reverse(a, start, end - d);
+		reverse(a, end - d + 1, end);
+		return a;
+
+	}
+
+	public static void reverse(int[] a, int start, int end) {
+		while (start < end) {
+			int temp = a[start];
+			a[start] = a[end];
+			a[end] = temp;
+			start++;
+			end--;
+
+		}
+
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] nd = scanner.nextLine().split(" ");
+
+		int n = Integer.parseInt(nd[0]);
+
+		int d = Integer.parseInt(nd[1]);
+
+		int[] a = new int[n];
+
+		String[] aItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int aItem = Integer.parseInt(aItems[i]);
+			a[i] = aItem;
+		}
+
+		int[] result = rotLeft(a, d);
+
+		for (int i = 0; i < result.length; i++) {
+			bufferedWriter.write(String.valueOf(result[i]));
+
+			if (i != result.length - 1) {
+				bufferedWriter.write(" ");
+			}
+		}
+
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+
+		scanner.close();
+		
+
+	}
+}
